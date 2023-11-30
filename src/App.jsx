ffbb7ac90f5c27a196dae38694db094b960a4b70
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-11-30 13:37:57 +0200

    side bar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import AuthPage from './pages/HomePage/AuthPage/AuthPage'
+import PageLayout from './Layouts/PageLayout/PageLayout'
 
 
 function App() {
 
 
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   )
 }
 
