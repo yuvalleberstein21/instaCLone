65f991c735a5f2191e2ed74c385e9ca3567548f9
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-12-02 11:25:50 +0200

    update

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,7 +1,9 @@
-import { Avatar, Box, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react'
+import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, VStack, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { AiFillHeart } from 'react-icons/ai'
 import { FaComment } from 'react-icons/fa'
+import { MdDelete } from 'react-icons/md'
+import Comment from '../Comment/Comment'
 
 const ProfilePost = ({ img }) => {
 
@@ -53,7 +55,6 @@ const ProfilePost = ({ img }) => {
             >
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody bg={"black"} pb={5}>
                         <Flex gap="4" w={{ base: "90%", sm: "70%", md: "full" }} mx={"auto"}>
@@ -66,13 +67,40 @@ const ProfilePost = ({ img }) => {
                             >
                                 <Image src={img} alt='Profile post' />
                             </Box>
-                            <Flex flex={1} flexDirection={"column"} px={10} display={{ base: "none", md: "flex" }}>
+                            <Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}>
                                 <Flex alignItems={"center"} justifyContent={"space-between"}>
-                                    <Avatar src='/profilepic.png' size={"sm"} name='As a Programmer' />
+                                    <Flex alignItems={"center"} gap={4}>
+                                        <Avatar src='/profilepic.png' size={"sm"} name='As a Programmer' />
+                                        <Text fontWeight={"bold"} fontSize={12}>asaprogrammer_</Text>
+                                    </Flex>
+                                    <Box _hover={{ bg: "whiteAlpha.300", color: "red.600" }} borderRadius={4} p={1}>
+                                        <MdDelete size={20} cursor={"pointer"} />
+                                    </Box>
                                 </Flex>
+                                <Divider my={4} bg={"gray.500"} />
+                                <VStack w={"full"} alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
+                                    <Comment
+                                        createdAt="1d ago"
+                                        username="yuval.programmer"
+                                        profilePic='/profilepic.png'
+                                        text={"Dumy images from unsplash"}
+                                    />
+                                    <Comment
+                                        createdAt="12h ago"
+                                        username="leber_IDF_"
+                                        profilePic='https://bit.ly/dan-abramov'
+                                        text={"Nice pic"}
+                                    />
+                                    <Comment
+                                        createdAt="3h ago"
+                                        username="messi.official"
+                                        profilePic='https://bit.ly/kent-c-dodds'
+                                        text={"Good clone dude!"}
+                                    />
+                                </VStack>
+                                <Divider my={4} bg={"gray.8000"} />
                             </Flex>
                         </Flex>
-
                     </ModalBody>
                 </ModalContent>
             </Modal>
